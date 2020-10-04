b554ec0fc30e043ce1400e30ff547fe971ba072c
Joshua Strozzi <strozzij@oregonstate.edu>
2020-10-04 01:56:36 -0700

    Add files via upload

---
+++ assign10/solutions/rna_semiring.py
@@ -0,0 +1,109 @@
+__author__ = "liang huang" # version 2, semiring
+
+from heapq import heapify, heappop, heappush
+from collections import defaultdict
+allowed = set(['AU','UA','CG','GC','GU','UG'])
+
+def common(s, plus, times1, times2, plus_unit, times_unit):
+
+    def update(i, j, value1, value2=None):
+        if value2 is None:
+            tot[i, j] = plus(tot[i, j], times1(value1))
+        else:
+            tot[i, j] = plus(tot[i, j], times2(value1, value2))
+
+    n = len(s)
+    tot = defaultdict(lambda: plus_unit)
+
+    for i in range(n+1): # N.B.: n+1: tot[n, n] = 1
+        tot[i, i+1] = times1(times_unit) # singleton '.'
+        tot[i, i] = times_unit # empty '' 
+
+    for span in range(2, n+1): # span length: 2..n
+        for i in range(n-span+1): # left boundary: 0..(n-span)
+            j = i+span # right boundary
+            
+            update(i, j, tot[i+1, j]) # case 1: i unpaired 
+
+            for k in range(i+1, j+1): # case 2: i paird with some k-1
+                if s[i]+s[k-1] in allowed: # i( ___ )k ____ j
+                    update(i, j, tot[i+1, k-1], tot[k, j])
+            
+    return tot[0, n]
+
+def total(s):
+    return common(s, lambda x,y: x+y, lambda x: x, lambda x,y: x*y, 0, 1) # (+, *, 0, 1) semiring
+
+def best(s):
+    return common(s, 
+                  lambda x,y: max(x,y), 
+                  lambda x: (x[0], ".%s" % x[1]),
+                  lambda x,y: (x[0]+y[0]+1, "(%s)%s" % (x[1], y[1])),
+                  (float("-inf"), ''), 
+                  (0, '')) # (max, +, -inf, 0) semiring
+
+def kbest(s, k): # using the division in total (no need to check duplicate), not in best
+    '''Huang and Chiang 2005 Algorithm 2'''
+
+    left = lambda m: opt[i+1, m-1]  # i ( xxx ) m ___ j
+    right = lambda m: opt[m, j]     # i ( ___ ) m xxx j
+    single = lambda : opt[i+1, j]   # i . xxxxxxxxxxx j
+
+    def tryadd(split, index1, index2): # i paired with split-1
+        if index1 < len(left(split)) and index2 < len(right(split)) \
+           and not (split, index1, index2) in used:
+            used.add((split, index1, index2))
+            heappush(heap, 
+                     (-left(split)[index1][0]-right(split)[index2][0]-1, split, index1, index2)) # pair
+
+    def tryadd1(index):
+        if index < len(single()):
+            heappush(heap, (-single()[index][0], index)) # min-heap, i unpaired
+
+    n = len(s)
+    opt = defaultdict(list) # list of (value, structure) pairs
+    for i in range(n+1):# N.B. opt[n,n]=0
+        opt[i, i+1] = [(0, '.')] # singleton
+        opt[i, i] = [(0, '')] # empty
+
+    for span in range(2, n+1): # span length: 2..n
+        for i in range(n-span+1): # left boundary: 0..(n-span)
+            j = i+span # right boundary
+
+            heap, used = [], set()
+
+            tryadd1(0) # i unpaired
+            for m in range(i+1, j+1): # i pair with some k
+                if s[i]+s[m-1] in allowed: # i( ___ )k ___ j
+                    tryadd(m, 0, 0)
+
+            heapify(heap)
+
+            while heap != [] and len(opt[i, j]) < k:
+                item = heappop(heap)
+                if len(item) == 2: # i unpaired
+                    value, index = item
+                    opt[i,j].append((-value, ".%s" % single()[index][1])) 
+                    tryadd1(index+1)
+                else: # i paired, split
+                    value, split, index1, index2 = item
+                    opt[i,j].append((-value, "(%s)%s" % (left(split)[index1][1], 
+                                                         right(split)[index2][1])))
+                    tryadd(split, index1+1, index2)
+                    tryadd(split, index1, index2+1)
+
+    return opt[0,n]#, solution(0, n)
+
+if __name__ == "__main__":
+    pp = lambda a: list(map(lambda n: n[0], a))    
+    print(best("ACAGU"))
+    print(total("ACAGU"))
+    print(kbest("ACAGU", 10))
+    print(pp(kbest("UCAGAGGCAUCAAACCU", 300)))
+    print(pp(kbest("CAUCGGGGUCUGAGAUGGCCAUGAAGGGCAC", 25)))
+    print(pp(kbest("AAACCAUCGGGGUCUGAGAUGGCCAUGAAGGGCA", 25))) 
+    print(pp(kbest("UUUUUCCCCCAUCGGGGUCUGAGAUGGCCAUGA", 25)))
+    print(pp(kbest("UUUUCCGGGGGGGGGCAUCGGGGUCUUUU", 25)))
+    print(pp(kbest("UUUCUCUCUCUCCAUCGGGGUCUGAGAUGGCCAUGAA", 35)))
+    print(pp(kbest("UCAGAGGCAUCAAACCUU", 700)))
+
